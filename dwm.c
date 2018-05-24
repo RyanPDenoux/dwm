3b9b0916cbb851085c7ac0bb9d83a27e7dacb79c
Christopher Drelich <cd@cdrakka.com>
2018-05-23 22:50:38 -0400

    Modifies quit to handle restarts and adds SIGHUP and SIGTERM handlers.
    Modified quit() to restart if it receives arg .i = 1
    MOD+CTRL+SHIFT+Q was added to confid.def.h to do just that.
    
    Signal handlers were handled for SIGHUP and SIGTERM.
    If dwm receives these signals it calls quit() with
    arg .i = to 1 or 0, respectively.
    
    To restart dwm:
    MOD+CTRL+SHIFT+Q
    or
    kill -HUP dwmpid
    
    To quit dwm cleanly:
    MOD+SHIFT+Q
    or
    kill -TERM dwmpid

--- dwm.c
+++ dwm.c
@@ -225,6 +225,8 @@ static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigchld(int unused);
 static void sigstatusbar(const Arg *arg);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void spawn(const Arg *arg);
 static int stackpos(const Arg *arg);
 static void tag(const Arg *arg);
@@ -290,6 +292,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[UnmapNotify] = unmapnotify
 };
 static Atom wmatom[WMLast], netatom[NetLast];
+static int restart = 0;
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
@@ -1402,6 +1405,7 @@ pushstack(const Arg *arg) {
 void
 quit(const Arg *arg)
 {
+	if(arg->i) restart = 1;
 	running = 0;
 }
 
@@ -1690,6 +1694,9 @@ setup(void)
 	/* clean up any zombies immediately */
 	sigchld(0);
 
+	signal(SIGHUP, sighup);
+	signal(SIGTERM, sigterm);
+
 	/* init screen */
 	screen = DefaultScreen(dpy);
 	sw = DisplayWidth(dpy, screen);
@@ -1801,6 +1808,20 @@ sigstatusbar(const Arg *arg)
 		return;
 
 	sigqueue(statuspid, SIGRTMIN+statussig, sv);
+}
+
+void
+sighup(int unused)
+{
+	Arg a = {.i = 1};
+	quit(&a);
+}
+
+void
+sigterm(int unused)
+{
+	Arg a = {.i = 0};
+	quit(&a);
 }
 
 void
@@ -2504,6 +2525,7 @@ main(int argc, char *argv[])
 #endif /* __OpenBSD__ */
 	scan();
 	run();
+	if(restart) execvp(argv[0], argv);
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
