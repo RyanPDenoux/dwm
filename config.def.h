263c4a8843bee5760f6f93c97786e9c8b7b4e15f
Ryan Denoux <ryanpdenoux@gmail.com>
2023-08-27 21:20:58 +0200

    Alignment

--- config.def.h
+++ config.def.h
@@ -9,28 +9,28 @@
 
 /* appearance */
 #include <X11/X.h>
-static unsigned int borderpx		 = 2;      /* border pixel of windows */
-static unsigned int snap			 = 32;     /* snap pixel */
-static const unsigned int gappih	 = 12;	   /* horizontal inner gap between windows */
-static const unsigned int gappiv	 = 12;	   /* vertical inner gap between windows */
-static const unsigned int gappoh	 = 12;	   /* horizontal outer gap between windows and screen edge */
-static const unsigned int gappov	 = 32;	   /* vertical outer gap between windows and screedn edge */
-static int smartgaps				 = 0;      /* 1 means no outer gap when there is only one window */
-static int swallowfloating			 = 0;      /* 1 means swallow floating windows by default */
-static int showbar					 = 1;      /* 0 means no bar */
-static int topbar					 = 1;      /* 0 means bottom bar */
-static const int horizpadbar		 = 4;      /* horizontal padding for statusbar */
-static const int vertpadbar			 = 8;      /* vertical padding for statusbar */
-static char font[]					 = "monospace:size=12";
-static char dmenufont[]				 = "monospace:size=12";
-static const char *fonts[]			 = { font };
-static char normbgcolor[]			 = "#002b36";
-static char normbordercolor[]		 = "#073642";
-static char normfgcolor[]			 = "#93a1a1";
-static char selfgcolor[]			 = "#eee8d5";
-static char selbordercolor[]		 = "#268bd2";
-static char selbgcolor[]			 = "#268bd2";
-static char *colors[][3]			 = {
+static unsigned int        borderpx          = 3; /* border pixel of windows */
+static unsigned int        snap              = 32; /* snap pixel */
+static const unsigned int  gappih            = 12; /* horizontal inner gap between windows */
+static const unsigned int  gappiv            = 12; /* vertical inner gap between windows */
+static const unsigned int  gappoh            = 12; /* horizontal outer gap between windows and screen edge */
+static const unsigned int  gappov            = 32; /* vertical outer gap between windows and screedn edge */
+static int                 smartgaps         = 0; /* 1 means no outer gap when there is only one window */
+static int                 swallowfloating   = 0; /* 1 means swallow floating windows by default */
+static int                 showbar           = 1; /* 0 means no bar */
+static int                 topbar            = 1; /* 0 means bottom bar */
+static const int           horizpadbar       = 4; /* horizontal padding for statusbar */
+static const int           vertpadbar        = 8; /* vertical padding for statusbar */
+static char                font[]            = "monospace:size=12";
+static char                dmenufont[]       = "monospace:size=12";
+static const char         *fonts[]           = { font };
+static char                normbgcolor[]	 = "#002b36";
+static char                normbordercolor[] = "#073642";
+static char                normfgcolor[]	 = "#93a1a1";
+static char                selfgcolor[]      = "#eee8d5";
+static char                selbordercolor[]  = "#268bd2";
+static char                selbgcolor[]      = "#268bd2";
+static char *colors[][3]					   = {
 	/*               fg           bg           border   */
 	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
@@ -42,8 +42,8 @@ static const int taglayouts[] = { 0, 2, 0, 0, 0, 0, 0, 0, 0 };
 
 static const Rule rules[] = {
 	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
+	 *  WM_CLASS(STRING)  = instance, class
+	 *  WM_NAME(STRING)   = title
 	 */
 	/* class      	     instance    title    tags mask  isfloating  CenterThisWindow?  isterminal  noswallow  monitor float x,y,w,h   floatborderpx */
 	{ "st",              NULL,       NULL,    0,         0,          1,                 1,          0,         -1, 	   50,50,500,500,  5 },
@@ -67,20 +67,20 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float     mfact          = 0.55; /* factor of master area size [0.05..0.95] */
+static int       nmaster        = 1; /* number of clients in master area */
+static int       resizehints    = 1; /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[]=",      tile },		/* first entry is default */
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "><>",      NULL },		/* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
